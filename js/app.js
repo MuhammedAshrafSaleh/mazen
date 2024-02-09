824e76c1f6713a92cf5f8f73c42926789b0c4525
Muhammed Ashraf <muhammedashrafsaleh@gmail.com>
2024-02-09 17:35:55 +0200

    Chnage Video Control

--- js/app.js
+++ js/app.js
@@ -85,29 +85,17 @@ document.addEventListener('DOMContentLoaded', () => {
   window.player3 = player3;
 });
 
-////////////////////////////
-// let map;
+var playerBtn1 = document.querySelector('.video1');
+var playerBtn2 = document.querySelector('.video2');
+var playerBtn3 = document.querySelector('.video3');
+playerBtn1.addEventListener('click', function(){
+  document.querySelector('.video1 .plyr__controls').style.display = 'block';
+});
+playerBtn2.addEventListener('click', function(){
+  document.querySelector('.video2 .plyr__controls').style.display = 'block';
+});
 
-// async function initMap() {
-//   // The location of Uluru
-//   const position = { lat: -25.344, lng: 131.031 };
-//   // Request needed libraries.
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+playerBtn3.addEventListener('click', function(){
+  document.querySelector('.video3 .plyr__controls').style.display = 'block';
+});
 
-//   // The map, centered at Uluru
-//   map = new Map(document.getElementById("map"), {
-//     zoom: 4,
-//     center: position,
-//     mapId: "DEMO_MAP_ID",
-//   });
-
-//   // The marker, positioned at Uluru
-//   const marker = new AdvancedMarkerElement({
-//     map: map,
-//     position: position,
-//     title: "Uluru",
-//   });
-// }
-// initMap();
